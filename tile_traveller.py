fd48ef39fa1a5ac9fe801d689b8473145e2ea765
Anna Sigrun <annasg19@ru.is>
2019-09-17 14:01:30 +0000

    fuck this

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,1 +1,7 @@
-#
+#0. Make a variable which holds the coordinates (hnit)
+#1. Print out where user can travel
+#2. Recive input from user
+#2.1 If input is not acceptable write out "Not a valid direction!"
+#3. Change coordinates according to input
+#3.1 If coordinates are the winning coordinates then print out Victory! and stop trunning
+#4. Repeat steps 1. to 3.
