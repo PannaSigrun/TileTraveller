a5f04750c0d130d87453dc65d74593cfde012bba
andriberg19 <andri19@ru.is>
2019-09-17 14:34:54 +0000

    Skeleton

--- tile_traveller.py
+++ tile_traveller.py
@@ -5,3 +5,48 @@
 #3. Change coordinates according to input
 #3.1 If coordinates are the winning coordinates then print out Victory! and stop trunning
 #4. Repeat steps 1. to 3.
+def user_input(x,y):
+    #fá input frá user
+    if input == "N" or input == "n":
+        return x, y += 1
+    elif input == "S" or input == "s":
+        return x, y -= 1
+    elif input == "E" or input == "e":
+        return x += 1, y
+    elif:
+    
+def start_tile():
+    print(print out where users can travel)
+    while True:
+        user_input = str(input("You can travel..."))
+        if user_input == "n" or user_input == 'N':
+            #keyrum férslufall
+            break
+        else:
+            print("Not a valid direction!")
+            user_input = str(input("You can travel..."))
+
+
+x,y = 1,1
+print(x,y)
+
+while pass:
+    if x == 1 and y == 1:
+        start_tile
+    elif x == 1 and y == 2:
+        fall 1,2
+    elif x == 1 and y == 3:
+        fall 1,3
+    elif x == 2 and y == 1:
+        fall 2,1
+    elif x == 2 and y == 2:
+        fall 2,2
+    elif x == 2 and y == 3:
+        fall 2,3
+    elif x == 3 and y == 1:
+        fall 3,1
+    elif x == 3 and y == 2:
+        fall 3,2
+    elif x == 3 and y == 3:
+        fall 3,3
+        break
